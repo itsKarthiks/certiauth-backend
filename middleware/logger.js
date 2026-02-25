b1b2854ae56f78ce5da8824a5edb74cde1f49750
Karthik S <karthikofficialg@gmail.com>
2026-02-25 20:01:40 +0530

    Delete middleware directory

--- middleware/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} request to ${req.url}`);
-    next(); // Move to the next step
-};
-
-module.exports = logger;
